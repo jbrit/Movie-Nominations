bab020ae09099bedcae9bfcb609c0bea56ba86cc
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-07 23:42:50 +0100

    Lookalike Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SearchBox from "./components/SearchBox";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container py-5">
+        <SearchBox />
+        <div className="row mt-5">
+          <div className="col-12 col-md-6 mb-5 mb-md-0">
+            <div className="card p-3">
+              <h6 className="mb-4"> Results for "ram"</h6>
+              <ul>
+                <li className="mb-3">
+                  Elt 1{" "}
+                  <button className="btn btn-light border" disabled>
+                    Nominate
+                  </button>
+                </li>
+                <li className="mb-3">
+                  Elt 2{" "}
+                  <button className="btn btn-light border">Nominate</button>
+                </li>
+                <li className="mb-3">
+                  Elt 3{" "}
+                  <button className="btn btn-light border">Nominate</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="card p-3">
+              <h6 className="mb-4">Nominations</h6>
+              <ul>
+                <li className="mb-3">
+                  Elt 1 <button className="btn btn-light border">Remove</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
