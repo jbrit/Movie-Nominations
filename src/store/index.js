6036e49b30b8c6475b8c635d2f4c3141c8371179
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-08 15:39:48 +0100

    Optional Redux, if it's available

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 
-export default createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
+const middleware = [applyMiddleware(thunk)];
+if (window.__REDUX_DEVTOOLS_EXTENSION__)
+  middleware.push(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  );
+export default createStore(rootReducer, compose(...middleware));
