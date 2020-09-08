3d36c9e412f8610c65a7f27c420f1a1a757621bf
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-08 02:34:34 +0100

    Redux + Correct API Setup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
 import React from "react";
 import SearchBox from "./components/SearchBox";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="container py-5">
-        <SearchBox />
-        <div className="row mt-5">
-          <div className="col-12 col-md-6 mb-5 mb-md-0">
-            <div className="card p-3">
-              <h6 className="mb-4"> Results for "ram"</h6>
-              <ul>
-                <li className="mb-3">
-                  Elt 1{" "}
-                  <button className="btn btn-light border" disabled>
-                    Nominate
-                  </button>
-                </li>
-                <li className="mb-3">
-                  Elt 2{" "}
-                  <button className="btn btn-light border">Nominate</button>
-                </li>
-                <li className="mb-3">
-                  Elt 3{" "}
-                  <button className="btn btn-light border">Nominate</button>
-                </li>
-              </ul>
+    <Provider store={store}>
+      <div className="App">
+        <div className="container py-5">
+          <SearchBox />
+          <div className="row mt-5">
+            <div className="col-12 col-md-6 mb-5 mb-md-0">
+              <div className="card p-3">
+                <h6 className="mb-4"> Results for "ram"</h6>
+                <ul>
+                  <li className="mb-3">
+                    Elt 1{" "}
+                    <button className="btn btn-light border" disabled>
+                      Nominate
+                    </button>
+                  </li>
+                  <li className="mb-3">
+                    Elt 2{" "}
+                    <button className="btn btn-light border">Nominate</button>
+                  </li>
+                  <li className="mb-3">
+                    Elt 3{" "}
+                    <button className="btn btn-light border">Nominate</button>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div className="col-12 col-md-6">
-            <div className="card p-3">
-              <h6 className="mb-4">Nominations</h6>
-              <ul>
-                <li className="mb-3">
-                  Elt 1 <button className="btn btn-light border">Remove</button>
-                </li>
-              </ul>
+            <div className="col-12 col-md-6">
+              <div className="card p-3">
+                <h6 className="mb-4">Nominations</h6>
+                <ul>
+                  <li className="mb-3">
+                    Elt 1{" "}
+                    <button className="btn btn-light border">Remove</button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
