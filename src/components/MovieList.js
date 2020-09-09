d2adcdd20c7a59f3e04aa61c4da9ced37e1155cc
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-09 15:27:57 +0100

    Refactored MovieList and NominationList fo functional components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,49 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { nominateMovie, getMovies } from "../actions/movieActions";
 
-class MovieList extends Component {
-  render() {
-    const { movies, nominations, search_param, nominateMovie } = this.props;
-    return (
-      <div className="card p-3">
-        <h6 className="mb-4"> Results for "{search_param}"</h6>
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.imdbID} className="mb-3">
-              <img
-                style={{
-                  objectFit: "contain",
-                  height: "100px",
-                  width: "100px",
-                }}
-                src={
-                  movie.Poster !== "N/A"
-                    ? movie.Poster
-                    : "https://bitsofco.de/content/images/2018/12/broken-1.png"
-                }
-                alt="Movie Poster Name"
-              />
-              {movie.Title} ({movie.Year})
-              <button
-                onClick={() => {
-                  nominateMovie(movie);
-                }}
-                className="btn btn-light border"
-                disabled={nominations.some(
-                  (elt) => elt.imdbID === movie.imdbID
-                )}
-              >
-                Nominate
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const MovieList = ({ movies, nominations, search_param, nominateMovie }) => {
+  return (
+    <div className="card p-3">
+      <h6 className="mb-4"> Results for "{search_param}"</h6>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.imdbID} className="mb-3">
+            <img
+              style={{
+                objectFit: "contain",
+                height: "100px",
+                width: "100px",
+              }}
+              src={
+                movie.Poster !== "N/A"
+                  ? movie.Poster
+                  : "https://bitsofco.de/content/images/2018/12/broken-1.png"
+              }
+              alt="Movie Poster Name"
+            />
+            {movie.Title} ({movie.Year})
+            <button
+              onClick={() => {
+                nominateMovie(movie);
+              }}
+              className="btn btn-light border"
+              disabled={nominations.some((elt) => elt.imdbID === movie.imdbID)}
+            >
+              Nominate
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 MovieList.propTypes = {
   movies: PropTypes.array.isRequired,
--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -1,53 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { removeMovie } from "../actions/movieActions";
 
-class NominationList extends Component {
-  static propTypes = {
-    nominations: PropTypes.array.isRequired,
-    removeMovie: PropTypes.func.isRequired,
-  };
+const NominationList = ({ nominations, removeMovie }) => {
+  return (
+    <div className="card p-3">
+      <h6 className="mb-4">Nominations</h6>
+      <ul>
+        {nominations.map((movie) => (
+          <li key={movie.imdbID} className="mb-3">
+            <img
+              style={{
+                objectFit: "contain",
+                height: "100px",
+                width: "100px",
+              }}
+              src={
+                movie.Poster !== "N/A"
+                  ? movie.Poster
+                  : "https://bitsofco.de/content/images/2018/12/broken-1.png"
+              }
+              alt="Movie Poster Name"
+            />
+            {movie.Title} ({movie.Year})
+            <button
+              onClick={() => {
+                removeMovie(movie);
+              }}
+              className="btn btn-light border"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-  componentWillMount() {}
-
-  render() {
-    const { nominations, removeMovie } = this.props;
-    return (
-      <div className="card p-3">
-        <h6 className="mb-4">Nominations</h6>
-        <ul>
-          {nominations.map((movie) => (
-            <li key={movie.imdbID} className="mb-3">
-              <img
-                style={{
-                  objectFit: "contain",
-                  height: "100px",
-                  width: "100px",
-                }}
-                src={
-                  movie.Poster !== "N/A"
-                    ? movie.Poster
-                    : "https://bitsofco.de/content/images/2018/12/broken-1.png"
-                }
-                alt="Movie Poster Name"
-              />
-              {movie.Title} ({movie.Year})
-              <button
-                onClick={() => {
-                  removeMovie(movie);
-                }}
-                className="btn btn-light border"
-              >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+NominationList.propTypes = {
+  nominations: PropTypes.array.isRequired,
+  removeMovie: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   nominations: state.movie.nominations,
