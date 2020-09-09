21ac36549231cb3c076175cf100bfd9754b03776
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-09 15:19:59 +0100

    Searchbox refactored to hooks

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,44 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getMovies } from "../actions/movieActions";
-class SearchBox extends Component {
-  state = {
-    movieTitle: "",
-  };
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-  handleSubmit = (e) => {
-    const { getMovies } = this.props;
-    e.preventDefault();
-    if (this.state.movieTitle) getMovies(this.state.movieTitle.trim());
-  };
-  render() {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="movieTitle">Movie Title</label>
-              <input
-                onChange={this.onChange}
-                name="movieTitle"
-                type="text"
-                className="form-control"
-                id="movieTitle"
-                placeholder="Example input placeholder"
-                autoFocus
-              />
-            </div>
-            <button className="btn btn-light border" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+const SearchBox = ({ getMovies }) => {
+  const [title, setTitle] = useState(""),
+    onChange = (e) => setTitle(e.target.value),
+    handleSubmit = (e) => {
+      e.preventDefault();
+      title && getMovies(title.trim());
+    };
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="movieTitle">Movie Title</label>
+            <input
+              onChange={onChange}
+              name="movieTitle"
+              type="text"
+              className="form-control"
+              id="movieTitle"
+              placeholder="Example input placeholder"
+              autoFocus
+            />
+          </div>
+          <button className="btn btn-light border" type="submit">
+            Search
+          </button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 SearchBox.propTypes = {
   getMovies: PropTypes.func.isRequired,
